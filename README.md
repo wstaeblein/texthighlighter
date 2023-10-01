a95d05aa999b3e7c32e285d7eaeaa8c5a4ee7f38
Walter Staeblein <walter@synergys.com.br>
2023-10-01 19:02:06 -0300

    Word search, Navigate results & results count

--- README.md
+++ README.md
@@ -7,15 +7,22 @@ Vanilla JS class to highlight search results in a textarea while maintaining the
 
 ## Features
 
-- No dependencies
+- No dependencies.
+- No configuration.
 - Can be used in as many textareas as needed, just instantiate for each one.
 - Responsive. The markings will adjust to screen size, textarea size and scroll.
-- Keeps the original background from the textarea
+- Keeps the original background from the textarea.
 - Can be properly destroyed. Will remove all events and structure.
 - Can choose between case sensitive and insensitive search.
+- Can serach freely or words only.
+- Search results count.
+- Search results navigation.
 
 
-Use this class if you need to highlight search results or any text in a textarea. Its use is very straightforward, instantiate the class for the textarea you need and call the search method to highlight the text you pass in the first argument. The second argument is optional and takes a boolean that if true will make the search case sensitive. The default is case insensitive.
+Since one can't style the text in a textarea, as it is not HTML, this class comes to the rescue allowing you to highlight any text in a textarea. Such as search results.
+
+Its use is very straightforward, instantiate the class for the textarea you need and call the search method to highlight the text you pass in the first argument. The second argument is optional and takes a boolean that if true will make the search case sensitive. The default is case insensitive.
+
 
 
 ## Usage & Code Examples
@@ -30,8 +37,25 @@ let tarea = document.getElementById('txt');
 let hilite = new textHighlight(tarea);
 
 let searchResult = 'Some Expression';
-let sens = true;                        // Optional
-hilite.search(searchResult, sens);
+let sens = true;                        // Case sensitive. Optional, default: false
+let word = true;                        // Perform a words only search. Optional, default: false
+hilite.search(searchResult, sens, word);
+```
+
+To access how many occurrences were found, use:
+
+```javascript
+let count = hilite.count();
+```
+
+You can navigate back and forth among the highlighted results using the prev and next methods:
+
+```javascript
+let btnPrev = document.getElementById('prev');
+let btnNext = document.getElementById('next');
+
+btnPrev.addEventListener('click', hilite.prev);
+btnNext.addEventListener('click', hilite.next);
 ```
 
 Should you need to clear out the highlights, call the clear method.
@@ -49,9 +73,11 @@ hilite.destroy();
 
 ## About
 
-This class was inspired by lonekorean's [highlight-within-textarea](https://github.com/lonekorean/highlight-within-textarea) JQuery plugin. Basically, I needed a similar functionality for a project but didn't want to include JQuery just for that and didn't find any other such code. 
+This class was inspired by lonekorean's [highlight-within-textarea](https://github.com/lonekorean/highlight-within-textarea) JQuery plugin. Basically, I needed a similar functionality for a project but didn't want to include JQuery just for that and didn't find any other such code that was good enough. 
 
-This class was tested in Chrome 110 and Firefox 110.
+The main aim here was to provide highlighting for search results in a textarea.
+
+This class was tested in Chrome 117 and Firefox 118.
 
 
 
--- texthighlighter.js
+++ texthighlighter.js
@@ -5,15 +5,15 @@
     https://github.com/wstaeblein/highlightTextarea
     *******************************************************/ 
 class textHighlight {
-
     constructor(ele) {
+        if (ele.tagName != 'TEXTAREA') { throw 'The element must be a textarea'; }
+
         let styles = window.getComputedStyle(ele);
         this.origBkgColor = styles.backgroundColor;
-
         this.ele = ele;
-        this.isTextArea = this.ele.tagName == 'TEXTAREA';
         this.searchArg = '';
         this.sensitive = false;
+        this.sel = -1;
 
         this.handlers = {
             input: this.#inputHandler.bind(this),
@@ -45,15 +45,34 @@ class textHighlight {
         this.#inputHandler();
     }
     
-    search(arg, sensitive) {
+    search(arg, sensitive, word) {
         this.searchArg = arg;
         this.sensitive = !!sensitive;
+        this.word = !!word;
         this.#inputHandler();
+        this.sel = -1;
+        this.next();
+    }
+
+    next() {
+        this.sel +=1;
+        this.#setSelection(true);
+    }
+
+    prev() {
+        this.sel -=1;
+        this.#setSelection(true);
+    }
+
+    count() {
+        let eles = this.hilite.querySelectorAll('mark');
+        return eles.length;
     }
 
     clear() { 
         this.searchArg  = '';
         this.hilite.innerHTML = this.hilite.textContent;
+        this.sel = -1;
     }
 
     destroy() {
@@ -69,7 +88,7 @@ class textHighlight {
 
     #resizeObs() {
         let styles = window.getComputedStyle(this.ele);
-        let width = this.ele.scrollWidth; console.log(this.ele.offsetWidth, ', ', this.ele.scrollWidth)
+        let width = this.ele.scrollWidth; 
         let height = this.ele.offsetHeight;
 
         let css = `width: ${width}px; height: ${height}px; margin: ${styles.marginTop} ${styles.marginRight} ${styles.marginBottom} ${styles.marginLeft}; 
@@ -78,12 +97,13 @@ class textHighlight {
         this.#copyStyles(this.ele, this.hilite, ['width', 'paddingLeft', 'paddingRight', 'paddingTop', 'paddingBottom', 'borderTop', 'letterSpacing',
                                                 'borderLeft', 'borderRight', 'borderBottom', 'fontFamily', 'fontSize', 'fontWeight', 'lineHeight']);
         this.hilite.style.minHeight = styles.height;
-        this.hilite.style.whiteSpace = this.isTextArea ? 'pre-wrap' : 'nowrap';
+        this.hilite.style.whiteSpace = 'pre-wrap';
     }
 
     #inputHandler() {
         this.hilite.innerHTML = this.#markText();
         this.#scrollHandler();
+        if (this.sel > -1) { this.#setSelection(); }
     }
 
     #copyStyles(src, dest, styles2Copy) {
@@ -92,25 +112,43 @@ class textHighlight {
     }
 
     #markText() { 
-        let txt = escape(this.isTextArea ? this.ele.value : this.ele.textContent);
-        if (this.searchArg) {
-            let re = new RegExp('(' + this.#escapeString(this.searchArg) + ')', 'g' + (this.sensitive ? '' : 'i')); 
-            return txt.replace(re, '<mark>$1</mark>');
+        if (this.searchArg) { console.log(this.ele.value)
+            let txt = this.ele.value.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+            let searchArg = this.searchArg.replace(/</g, '&lt;').replace(/>/g, '&gt;');
+            let boundary = this.word ? '\\b' : '';
+
+            let re = new RegExp(boundary + '(' + this.#escapeString(searchArg) + ')' + boundary, 'g' + (this.sensitive ? '' : 'i')); 
+            return txt.replace(re, '<mark>$&</mark>');
         } else {
             return txt;
         }
     }
 
-    // Escape a string to be used in a Regex search (replace)
     #escapeString(txt) {
         let specials = ['-', '[', ']', '/', '{', '}', '(', ')', '*', '+', '?', '.', '\\', '^', '$', '|'];
-        let regex = RegExp('[' + specials.join('\\') + ']', 'g'); 
-        return txt.replace(regex, '\\$&'); 
+        return txt.replace(RegExp('[' + specials.join('\\') + ']', 'g'), '\\$&'); 
     }
 
     #scrollHandler() { 
         this.backdrop.scrollTop = this.ele.scrollTop || 0; 
         let sclLeft = this.ele.scrollLeft;
         this.backdrop.style.transform = (sclLeft > 0) ? 'translateX(' + -sclLeft + 'px)' : '';
+    }
+
+    #setSelection(scroll) {
+        let eles = this.hilite.querySelectorAll('mark');
+        let len = eles.length;
+
+        if (this.sel >= len) { this.sel = 0; }
+        if (this.sel < 0) { this.sel = len - 1; }
+
+        for (let i = 0; i < len; ++i) {
+            if (i == this.sel) {
+                eles[i].classList.add('sel');
+                if (scroll) { this.ele.scrollTop = eles[i].offsetTop > 10 ? eles[i].offsetTop - 10 : eles[i].offsetTop; }
+            } else {
+                eles[i].classList.remove('sel');
+            }
+        }
     }
 }
